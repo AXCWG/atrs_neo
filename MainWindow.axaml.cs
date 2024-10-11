c10b68aaa6da09a24103a006a78ea23758e372f2
axcwg <xiey0@qq.com>
2024-10-12 01:30:18 +0800

    all

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+namespace Launcher
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using System;
+
+namespace Launcher
+{
+    class Launcher
+    {
+        public Launcher() { }
+    }
+
+    internal class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            System.GC.Collect();
+            return AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
+        }
+       
+    }
+}
